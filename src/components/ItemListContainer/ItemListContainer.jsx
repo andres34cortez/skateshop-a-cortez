e30cd170a5f779a82070ae22b76c80f97a7005e7
Andres <andrescel34@gmail.com>
2022-06-23 16:34:00 -0300

    entrega 5 final

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -9,13 +9,13 @@ import "./ItemListContainer.css";
 export default function ItemListContainer({ saludo }) {
   let inicial = 1;
   let stock = 10;
-  function Cargado(numero) {
+  function onAdd(numero) {
     alert(`${numero}`); // hago un alert como para mostrarlo, despues esto va a hacer la carga de el carrito
   }
   return (
     <Col className="ILCcontainer">
       <h1>{saludo}</h1>
-      <ItemList initial={inicial} stock={stock} Cargado={Cargado}></ItemList>
+      <ItemList initial={inicial} stock={stock} Cargado={onAdd}></ItemList>
      {/*  <ItemCount initial={inicial} stock={stock} Cargado={Cargado}></ItemCount> */}
     </Col>
   );
